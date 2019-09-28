85fb8fd5943d7fb018b24be1345608d9430f2f06
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-09-28 14:35:08 -0400

    MVP done?

--- index.js
+++ index.js
@@ -2,46 +2,42 @@ $(document).ready(function() {
     $('select').niceSelect();
 });
 
-function getResults() {
-  $("#js-section-results").append(
-    `<div>
-        <h4>Results</h4>
-      
-        <ul>
-          <li>Dummy results</li>
-          <li>Dummy results</li>
-          <li>Dummy results</li>
-          <li>Dummy results</li>
-        </ul>
-    </div>`
-  );
-}
+let searchTerm = ''
 
-function watchFormForSelectLanguage() {
-
+function watchForm() {
   $("#js-select-language").change(e => {
     e.preventDefault(); 
 
     $('#js-screen-for-select-language').hide()
     $('#js-screen-for-search-term').show()
 
-    //let selectedLanguage = $("select").val();        
-   
-    //getResults();
-  })
+    $("#form-input").submit(e => {
+      e.preventDefault(); 
+      
+    searchTerm = document.querySelector("#search-input").value;
+    getResults(searchTerm);
+    })    
+  });
 }
 
-function watchFormForSearchInput() {
+function getResults(searchTerm) {
+  
+    if(searchTerm == "I want to add two numbers") {
+      document.querySelector("#js-section-results").innerHTML = "addNumbers"
+    }
 
-  $("#btn-go-get-them").submit(e => {
-    e.preventDefault();
+    if(searchTerm == "I want to draw a circle") {
+      document.querySelector("#js-section-results").innerHTML = "drawCircle"
+    }
+    
+    if(searchTerm == "List all my favourite books") {
+      document.querySelector("#js-section-results").innerHTML = "favBookList"
+    }
 
-    getResults()
-  })
+    if(searchTerm == "List all my grades for year 2018-2019") {
+      document.querySelector("#js-section-results").innerHTML = "grades2018-2018"
+    }
 }
 
+watchForm();
 
-watchFormForSearchInput()
-watchFormForSelectLanguage()
-
-// edge cases!!!
